b92d324537e9be881e10c6e8c2b50edd7de4c271
Jonas Silva <jonas.silva502@gmail.com>
2024-07-08 23:39:48 -0300

    Pequena correção ao cadastrar Usuario

--- src/controller/cadastraUsuario.php
+++ src/controller/cadastraUsuario.php
@@ -9,8 +9,8 @@
         $nome = $_POST['nomeUsuario'];
         $email = $_POST['emailUsuario'];
         $senha = password_hash($_POST['senha'], PASSWORD_BCRYPT);
-        $cargo = $_POST['cargoUsuario'];
-        $ativo = $_POST['ativoUsuario'];
+        $cargo = $_POST['nomePerfil'];
+        $ativo = $_POST['ativo'];
 
         $user = new Usuario($nome, $email, $cargo, $ativo, $senha);
         $repositorio = new UsuarioRepositorio();
