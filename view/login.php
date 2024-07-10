176abaff227ed2ffa9746ec094ac08dbe3b01334
Jonas Silva <jonas.silva502@gmail.com>
2024-07-10 00:03:30 -0300

    Front atualizado e correção de bugs

--- view/login.php
+++ view/login.php
@@ -12,8 +12,8 @@ session_start();
     <link rel="stylesheet" href="./estilos/pg-login.css">
     <title>Login</title>
 </head>
-
 <body class="h100">
+    
     <form class="formulario centralizado mt12-5" action="../src/controller/logando.php" method="post">
         <h1 class="formulario__titulo centralizado">Entrar</h1>
         <fieldset class="formulario__campo w80 ">
@@ -34,10 +34,8 @@ session_start();
             </section>
 
         </fieldset>
-        <div class="centralizado mv0-5 grid50-50 w50">
-            <a href="./cadastrar_usuario.php"
-                class="formulario__botao__secundario formulario__botao__padrao">Cadastrar</a>
-            <input type="submit" name="login" value="Logar" class="formulario__botao formulario__botao__padrao ">
+        <div class="centralizado mv0-5 grid100 w50">
+            <input type="submit" value="Logar" name="login" class="formulario__botao formulario__botao__padrao ">
         </div>
     </form>
     <script src="https://kit.fontawesome.com/df85906e6a.js" crossorigin="anonymous"></script>
